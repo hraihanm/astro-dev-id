e6dacb5fc78c9e332d2afd825a7fbca504d0e557
Hammam Raihan M <43190450+hraihanm@users.noreply.github.com>
2025-12-07 11:08:19 +0700

    refactor: Replace Node adapter with Vercel adapter in Astro configuration and update package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,21 +32,13 @@
 // });
 
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   integrations: [tailwind()],
-  server: {
-    port: 3000,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  },
   vite: {
     server: {
       hmr: {
